ab8393128dcd440ef4b41f3ee098f38c8db2c835
hendraw <emailhendra2@gmail.com>
2022-06-12 13:46:08 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 docker stop face_api
 docker rm face_api
+docker rmi $(docker images 'face_api' -a -q)
 docker build -t face_api .
 docker run -d --name face_api -p 9090:9090  --restart unless-stopped face_api
